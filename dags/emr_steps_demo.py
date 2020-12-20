57d90af8fd278a4e141b7c7a2f95322af3462d6e
Gary Stafford <gary.a.stafford@gmail.com>
2020-12-20 11:49:19 -0500

    Initial commit of project

---
+++ dags/emr_steps_demo.py
@@ -0,0 +1,99 @@
+import os
+from datetime import timedelta
+
+from airflow import DAG
+from airflow.contrib.operators.emr_add_steps_operator import EmrAddStepsOperator
+from airflow.contrib.operators.emr_create_job_flow_operator import EmrCreateJobFlowOperator
+from airflow.contrib.sensors.emr_step_sensor import EmrStepSensor
+from airflow.models import Variable
+from airflow.utils.dates import days_ago
+
+# ************** AIRFLOW VARIABLES **************
+airflow_email = Variable.get('airflow_email')
+release_label = Variable.get('release_label')
+emr_ec2_key_pair = Variable.get('emr_ec2_key_pair')
+# ***********************************************
+
+DAG_ID = os.path.basename(__file__).replace('.py', '')
+
+DEFAULT_ARGS = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email': ['{{ var.value.airflow_email }}'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+}
+
+SPARK_STEPS = [
+    {
+        'Name': 'calculate_pi',
+        'ActionOnFailure': 'CONTINUE',
+        'HadoopJarStep': {
+            'Jar': 'command-runner.jar',
+            'Args': ['/usr/lib/spark/bin/run-example', 'SparkPi', '10'],
+        },
+    }
+]
+
+JOB_FLOW_OVERRIDES = {
+    'Name': 'demo-airflow-cluster',
+    'ReleaseLabel': '{{ var.value.release_label }}',
+    'Applications': [
+        {
+            'Name': 'Spark'
+        },
+    ],
+    'Instances': {
+        'InstanceGroups': [
+            {
+                'Name': 'Master nodes',
+                'Market': 'ON_DEMAND',
+                'InstanceRole': 'MASTER',
+                'InstanceType': 'm5.xlarge',
+                'InstanceCount': 1,
+            },
+            {
+                'Name': 'Slave nodes',
+                'Market': 'ON_DEMAND',
+                'InstanceRole': 'CORE',
+                'InstanceType': 'm5.xlarge',
+                'InstanceCount': 2,
+            }
+        ],
+        'KeepJobFlowAliveWhenNoSteps': False,
+        'TerminationProtected': False,
+        'Ec2KeyName': '{{ var.value.emr_ec2_key_pair }}',
+    },
+    'VisibleToAllUsers': True,
+    'JobFlowRole': 'EMR_EC2_DefaultRole',
+    'ServiceRole': 'EMR_DefaultRole'
+}
+
+with DAG(
+        dag_id=DAG_ID,
+        default_args=DEFAULT_ARGS,
+        dagrun_timeout=timedelta(hours=2),
+        start_date=days_ago(1),
+        schedule_interval='@once',
+        tags=['emr'],
+) as dag:
+    cluster_creator = EmrCreateJobFlowOperator(
+        task_id='create_job_flow',
+        job_flow_overrides=JOB_FLOW_OVERRIDES
+    )
+
+    step_adder = EmrAddStepsOperator(
+        task_id='add_steps',
+        job_flow_id="{{ task_instance.xcom_pull(task_ids='create_job_flow', key='return_value') }}",
+        aws_conn_id='aws_default',
+        steps=SPARK_STEPS,
+    )
+
+    step_checker = EmrStepSensor(
+        task_id='watch_step',
+        job_flow_id="{{ task_instance.xcom_pull('create_job_flow', key='return_value') }}",
+        step_id="{{ task_instance.xcom_pull(task_ids='add_steps', key='return_value')[0] }}",
+        aws_conn_id='aws_default',
+    )
+
+    cluster_creator >> step_adder >> step_checker
