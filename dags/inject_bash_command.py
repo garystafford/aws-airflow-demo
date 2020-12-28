38c1351fc7c3e908703e019446d246055d2fe5d3
Gary Stafford <gary.a.stafford@gmail.com>
2020-12-28 13:41:04 -0500

    New DAG to inject ad-hoc bash commands

---
+++ dags/inject_bash_command.py
@@ -0,0 +1,32 @@
+import os
+from datetime import timedelta
+
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.utils.dates import days_ago
+
+DAG_ID = os.path.basename(__file__).replace('.py', '')
+
+DEFAULT_ARGS = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False
+}
+
+with DAG(
+        dag_id=DAG_ID,
+        default_args=DEFAULT_ARGS,
+        description='Run a dynamic Bash command',
+        dagrun_timeout=timedelta(hours=2),
+        start_date=days_ago(1),
+        schedule_interval=None,
+        tags=['bash']
+) as dag:
+    list_python_packages_operator = BashOperator(
+        task_id='list_python_packages',
+        bash_command="{{ dag_run.conf['bash_command'] }}"
+    )
+
+list_python_packages_operator
