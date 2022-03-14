8416204fcf095a74f78bd1cd3b8e18c9e11e0aee
Gary A. Stafford <garystafford@users.noreply.github.com>
2022-03-13 22:51:39 -0400

    Update lineage_emission.py

--- dags/airflow_v2/lineage_emission.py
+++ dags/airflow_v2/lineage_emission.py
@@ -1,5 +1,8 @@
 """Lineage Emission
+
 This example demonstrates how to emit lineage to DataHub within an Airflow DAG.
+Also, lists contents of contents of airflow.cfg and all env vars.
+
 Based on reference DataHub DAG:
 https://github.com/linkedin/datahub/blob/master/metadata-ingestion/src/datahub_provider/example_dags/lineage_emission_dag.py
 """
@@ -9,12 +12,30 @@ from datetime import timedelta
 
 import datahub.emitter.mce_builder as builder
 from airflow import DAG
+from airflow.models.baseoperator import chain
+from airflow.operators.python import PythonOperator
 from airflow.utils.dates import days_ago
 from datahub_provider.operators.datahub import DatahubEmitterOperator
 
 # override airflow.cfg file's properties in MWAA
 os.environ["AIRFLOW__LINEAGE__BACKEND"] = "datahub_provider.lineage.datahub.DatahubLineageBackend"
-os.environ["AIRFLOW__LINEAGE__DATAHUB_KWARGS"] = '{"datahub_conn_id": "datahub_rest", "cluster": "dev", "capture_ownership_info": true, "capture_tags_info": true, "graceful_exceptions": true }'
+os.environ["AIRFLOW__LINEAGE__DATAHUB_KWARGS"] = \
+    '{"datahub_conn_id":"datahub_rest","cluster":"dev","capture_ownership_info":true,"capture_tags_info":true,"graceful_exceptions":true}'
+
+DAG_ID = os.path.basename(__file__).replace(".py", "")
+
+
+# print contents of airflow.cfg
+def print_airflow_cfg():
+    with open(f"{os.getenv('AIRFLOW_HOME')}/airflow.cfg", "r") as airflow_cfg:
+        file_contents = airflow_cfg.read()
+        print(f"\n{file_contents}")
+
+
+# print all env vars
+def print_env_vars():
+    print("\n".join([f"{k}: {v}" for k, v in sorted(os.environ.items())]))
+
 
 default_args = {
     "owner": "airflow",
@@ -28,12 +49,15 @@ default_args = {
 }
 
 with DAG(
-        "datahub_lineage_emission_example",
+        dag_id=DAG_ID,
         default_args=default_args,
         description="An example DAG demonstrating lineage emission within an Airflow DAG.",
-        schedule_interval=timedelta(days=1),
-        start_date=days_ago(2),
+        schedule_interval=None,
+        start_date=days_ago(1),
         catchup=False,
+        dagrun_timeout=timedelta(minutes=5),
+        tags=["datahub demo"],
+
 ) as dag:
     emit_lineage_task = DatahubEmitterOperator(
         task_id="emit_lineage",
@@ -41,14 +65,24 @@ with DAG(
         mces=[
             builder.make_lineage_mce(
                 upstream_urns=[
-                    builder.make_dataset_urn("redshift", "mydb.schema.tableA"),
-                    builder.make_dataset_urn("redshift", "mydb.schema.tableB"),
+                    builder.make_dataset_urn("glue", "synthea_patient_datahub.patients"),
+                    builder.make_dataset_urn("glue", "synthea_patient_datahub.conditions"),
                 ],
                 downstream_urn=builder.make_dataset_urn(
-                    "redshift", "mydb.schema.tableC"
-                ),
+                    "glue", "synthea_patient_datahub.sinusitis_glue",
+                )
             )
         ],
     )
+    get_airflow_cfg_operator = PythonOperator(
+        task_id="get_airflow_cfg_task", python_callable=print_airflow_cfg
+    )
+    get_print_env_vars_operator = PythonOperator(
+        task_id="get_print_env_vars_task", python_callable=print_env_vars
+    )
 
-    emit_lineage_task
+chain(
+    emit_lineage_task,
+    get_airflow_cfg_operator,
+    get_print_env_vars_operator
+)
